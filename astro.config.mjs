10ad967f6282766cb9076cb7bbe404c79f2525d1
Grant <gergely.daniel7@gmail.com>
2024-07-13 19:40:48 +0200

    Remove preact for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import { defineConfig } from "astro/config";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
 
-import preact from "@astrojs/preact";
-
 // https://astro.build/config
 export default defineConfig({
-    integrations: [preact()],
+    integrations: [],
     markdown: {
         rehypePlugins: [
             rehypeSlug,
