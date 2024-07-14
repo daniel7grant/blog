95b41226e82f191418db711560c4836864f1ad37
Grant <gergely.daniel7@gmail.com>
2024-07-14 02:10:13 +0200

    Add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,21 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [],
+    site: "https://danielgrants.com",
+    integrations: [sitemap()],
     markdown: {
         rehypePlugins: [
             rehypeSlug,
-            [rehypeAutolinkHeadings, { behavior: "append" }],
+            [
+                rehypeAutolinkHeadings,
+                {
+                    behavior: "append",
+                },
+            ],
         ],
     },
 });
