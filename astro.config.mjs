7101e8421fb82efc0a616181f8f26579f292a9c3
Grant <gergely.daniel7@gmail.com>
2026-07-23 22:44:15 +0200

    Upgrade Astro to v7

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import { unified } from "@astrojs/markdown-remark";
 import sitemap from "@astrojs/sitemap";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
@@ -8,14 +9,16 @@ export default defineConfig({
     site: "https://danielgrants.com",
     integrations: [sitemap()],
     markdown: {
-        rehypePlugins: [
-            rehypeSlug,
-            [
-                rehypeAutolinkHeadings,
-                {
-                    behavior: "append",
-                },
+        processor: unified({
+            rehypePlugins: [
+                rehypeSlug,
+                [
+                    rehypeAutolinkHeadings,
+                    {
+                        behavior: "append",
+                    },
+                ],
             ],
-        ],
+        }),
     },
 });
